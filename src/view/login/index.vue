5a35eb253fee1c971822505e0d9b93406942172b
zhongwen5 <zhong_1128wen@163.com>
2019-05-08 17:14:38 +0800

    all source

---
+++ src/view/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sa-login">
+      <Service/>
+  </div>
+</template>
+<script>
+import Service from './components/service';
+export default {
+    components:{
+        Service
+    },
+    data(){
+        return {
+                
+        }
+    }
+}
+</script>
